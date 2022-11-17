1ab6490297ed7e5e5994ecb000b45b3db9eac603
Abhishek <frequent.designer@frequentresearch.com>
2022-11-17 10:55:42 +0530

    vendor credit form

---
+++ temp/js/main.js
@@ -0,0 +1,59 @@
+$("body").on("keyup", "form", function(e){
+    if (e.which == 13){
+      if ($("#next").is(":visible") && $("fieldset.current").find("input, textarea").valid() ){
+        e.preventDefault();
+        nextSection();
+        return false;
+      }
+    }
+  });
+   
+   
+  $("#next").on("click", function(e){
+    console.log(e.target);
+    nextSection();
+  });
+   
+  $("form").on("submit", function(e){
+    if ($("#next").is(":visible") || $("fieldset.current").index() < 3){
+      e.preventDefault();
+    }
+  });
+   
+  function goToSection(i){
+    $("fieldset:gt("+i+")").removeClass("current").addClass("next");
+    $("fieldset:lt("+i+")").removeClass("current");
+    $("li").eq(i).addClass("current").siblings().removeClass("current");
+    setTimeout(function(){
+      $("fieldset").eq(i).removeClass("next").addClass("current active");
+        if ($("fieldset.current").index() == 3){
+          $("#next").hide();
+          $("input[type=submit]").show();
+        } else {
+          $("#next").show();
+          $("input[type=submit]").hide();
+        }
+    }, 80);
+   
+  }
+   
+  function nextSection(){
+    var i = $("fieldset.current").index();
+    if (i < 3){
+      $("li").eq(i+1).addClass("active");
+      goToSection(i+1);
+    }
+  }
+   
+  $("li").on("click", function(e){
+    var i = $(this).index();
+    if ($(this).hasClass("active")){
+      goToSection(i);
+    } else {
+      alert("Please complete previous sections first.");
+    }
+  });
+   
+   $('.form-group .select-input .input-field').click(function(){
+    $(this).parent().children('ul').slideToggle();
+   });
